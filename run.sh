297d1222cfd481fa448b7de2089035d497bf96b1
Shuzheng Si <ssz24@mails.tsinghua.edu.cn>
2026-04-30 10:35:40 +0800

    update

--- run.sh
+++ run.sh
@@ -9,29 +9,29 @@ export OPENAI_API_KEY=""
 INPUT="./CL-bench-context-dedup.jsonl"
 
 python selfplay_loop.py \
-    --challenger-model gpt-5.1 \
-    --reasoner-model gpt-5.1 \
+    --challenger-model gpt-4.1 \
+    --reasoner-model gpt-4.1 \
     --judge-model gpt-5.1 \
-    --proposer-model gpt-5.1 \
-    --generator-model gpt-5.1 \
+    --proposer-model gpt-4.1 \
+    --generator-model gpt-4.1 \
     --input "$INPUT" \
-    --output outputs/loop_data/loop_gpt-5.1-judge5-1.jsonl \
+    --output outputs/loop_data/loop_gpt-4.1-judge5-1.jsonl \
     --num-iterations 5 \
     --num-tasks 5 \
-    --skills-dir skills-from-5.1-judge5-1 \
+    --skills-dir skills-from-4.1-judge5-1 \
     --workers 32
 
 
 INPUT="./CL-bench-with-task-delimiter.jsonl"
 
 python infer.py \
-    --model gpt-5.1 \
+    --model gpt-4.1 \
     --input "$INPUT" \
     --workers 32 \
-    --skills-dir skills-from-5.1-judge5-1/reasoner \
-    --output outputs/gpt-5.1-skills-from-5.1-judge5-1.jsonl
+    --skills-dir skills-from-4.1-judge5-1/reasoner \
+    --output outputs/gpt-4.1-skills-from-4.1-judge5-1.jsonl
 
 python eval_ignore_none.py \
-    --input outputs/gpt-5.1-skills-from-5.1-judge5-1.jsonl \
+    --input outputs/gpt-4.1-skills-from-4.1-judge5-1.jsonl \
     --judge-model gpt-5.1 \
     --workers 32
